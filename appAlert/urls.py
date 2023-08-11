8262d8cd6faec770bb6f4d5ebbc756c25a2d461c
Halleluyah <halleluyah1000000@gmail.com>
2023-08-11 13:25:36 +0900

    first commit

---
+++ appAlert/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+                #   path('send_notifications/', views.sendNotifications, name='send_notifications'),
+                #   path('send_push/', views.send_push, name='send_push'),
+                    path('test_send/', views.test_send, name='test_send'),
+                    path('register_device/', views.register_device, name='register_device'),
+              ] 
---
+++ fcm_nonitication/local.py
@@ -0,0 +1,12 @@
+
+cred = 'C:\\Users\\Hale\\Desktop\\New folder\\Summer 2023\\copy\\fcm_django_api_notification\\serviceAccountKey.json'
+
+
+
+FCM_DJANGO_SETTINGS = {
+    "FCM_SERVER_KEY": "BMoHD2_MFPJBuqJIEO5n7uxpbaBT8T9eyDsBBONGZMYYaHAevVZ4sStX9GQDk8lmgFtE1zRnFQn1AQFg1eGlFe0",
+    # true if you want to have only one active device per registered user at a time
+    # default: False
+}
+
+FCM_API_KEY="AIzaSyDmnXhzB_aRqfjZ4-Fus63vSXXp6csfyMI"
